a9b4bda7ddc4a22cd0ac2ef4ecd576fee1ae9074
MrFtyoQr <quintana.real.jqr@gmail.com>
2025-03-23 22:34:02 -0600

    ULTIMA ESPERANZA

--- infrastructure/openrouter_client.py
+++ infrastructure/openrouter_client.py
@@ -4,7 +4,7 @@ import random  # importamos random para generar la sopa de letras
 
 class OpenRouterClient:  # aqui creamos la clase OpenRouterClient
     API_URL = "https://openrouter.ai/api/v1/chat/completions"
-    API_KEY = "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"
+    API_KEY = "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"
     # aqui creamos las variables API_URL y API_KEY, API_URL es la url a la que haremos la peticion y API_KEY es la clave que nos permitira hacer la peticion
 
     def get_response(self, question: str):  # aqui creamos el metodo get_response que recibe un parametro question
